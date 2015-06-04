1ebd206729f762f8ebe95dd2297c0a78e25a0a07
Allen Chang <iafayte@gmail.com>
2015-06-04 17:41:36 -0500

    stringifyJSON tests working

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,39 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(obj === undefined){
+    return null;
+  }else if(typeof(obj) === 'function'){
+    return null;
+  }else if(obj === null){
+    return 'null';
+  }else if(typeof(obj) === 'string'){
+    return '"' + obj + '"';
+  }else if(typeof(obj) === 'number'){
+    return obj.toString();
+  }else if(typeof(obj) == 'boolean'){
+    return obj.toString();
+  }else if(Array.isArray(obj)){        // array case
+    if(obj.length === 0) return '[]';
+    else{
+      return '[' + 
+        obj.map(function(ele){
+          return stringifyJSON(ele);
+      }).join(',') + ']';
+    }
+  }else{
+    // object case
+    var result = [];
+    keys = Object.keys(obj);
+
+    keys.forEach(function(key){
+      var value = stringifyJSON(obj[key]); // get the string value of the obj through recursion
+      
+      if(value !== null){
+        result.push('"' + key + '":' + value);
+      }
+    });
+
+    return "{" + result.join(",") + "}";
+  }
 };
